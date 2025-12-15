9616df6b83fd8a4258acc53df309a7772382cb41
BogdanDolia <cr.co.erph@gmail.com>
2025-12-15 22:46:59 +0200

    Refactor main function to improve structure and update content generation logic

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import os
 import argparse
+
 from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
+from google.genai import types
 
 
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        model="gemini-2.5-flash", contents=messages
     )
     if response.usage_metadata is not None:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
